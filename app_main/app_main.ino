237a33486d80a988b3833c71c6a058d5e65c2679
Gary Bake <gary.bake@planixs.com>
2016-04-20 21:34:48 +0100

    First commit

---
+++ app_main/app_main.ino
@@ -0,0 +1,80 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
+const char* ssid = "SECRET";
+const char* password = "SECRET";
+
+ESP8266WebServer server(80);
+
+const int led = 2;
+
+void handleLeftpad() {
+  digitalWrite(led, 0);
+  String outStr = server.arg("input");
+  int padCount = server.arg("padcount").toInt();
+  String ch = server.arg("char");
+
+  for(int a = 0; a < padCount; a++ ){
+      outStr = ch + outStr;
+  }
+  
+  server.send(200, "text/plain", outStr);
+  digitalWrite(led, 1);
+}
+
+void handleNotFound(){
+  digitalWrite(led, 1);
+  String message = "File Not Found\n\n";
+  message += "URI: ";
+  message += server.uri();
+  message += "\nMethod: ";
+  message += (server.method() == HTTP_GET)?"GET":"POST";
+  message += "\nArguments: ";
+  message += server.args();
+  message += "\n";
+  for (uint8_t i=0; i<server.args(); i++){
+    message += " " + server.argName(i) + ": " + server.arg(i) + "\n";
+  }
+  server.send(404, "text/plain", message);
+  digitalWrite(led, 0);
+}
+
+void setup(void){
+  pinMode(led, OUTPUT);
+  digitalWrite(led, 0);
+  Serial.begin(115200);
+  WiFi.begin(ssid, password);
+  Serial.println("");
+
+  // Wait for connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+
+  if (MDNS.begin("esp8266")) {
+    Serial.println("MDNS responder started");
+  }
+
+  server.on("/leftpad", handleLeftpad);
+
+  server.on("/", [](){
+    server.send(200, "text/plain", "usage /leftpad?input=superman&padcount=3&char=_");
+  });
+
+  server.onNotFound(handleNotFound);
+
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+}
